ac65b9bf5bcec325287a1de8984c37d57713ed67
Ed Hamilton <madebyeggs@gmail.com>
2018-03-06 10:32:45 +0000

    remove refs to deleted models

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,7 @@ Rails.application.routes.draw do
   root 'slides#index'
   
   resources :announcements
-  resources :clogos
   resources :announcements do
-    post :update_row_order, on: :collection
-    collection {post :import}
-  end
-  resources :artists
-  resources :artists do
     post :update_row_order, on: :collection
     collection {post :import}
   end
@@ -21,34 +15,9 @@ Rails.application.routes.draw do
     post :update_row_order, on: :collection
     collection {post :import}
   end
-  resources :releases
-  resources :slides, :path => "news"
-  resources :releases do
-    post :update_row_order, on: :collection
-    collection {post :import}
-  end
-  resources :customs, :path => "composition"
-  resources :customs do
-    post :update_row_order, on: :collection
-    collection {post :import}
-  end
-  resources :researches, :path => "researchlondon"
-  resources :researches do
-    post :update_row_order, on: :collection
-    collection {post :import}
-  end
-  resources :contacts, :path => "contact"
-  resources :share_closes
-  resources :newsletters
   
-  match "admins/artists" => "admins#artists", via: [:get, :post]
-  match "admins/customs" => "admins#customs", via: [:get, :post]
   match "admins/placements" => "admins#placements", via: [:get, :post]
-  match "admins/releases" => "admins#releases", via: [:get, :post]
-  match "admins/customs" => "admins#customs", via: [:get, :post]
-  match "admins/researches" => "admins#researches", via: [:get, :post]
   match "admins/slideshow" => "admins#slideshow", via: [:get, :post]
   match "admins/announcements" => "admins#announcements", via: [:get, :post]
-  match "admins/newsletter" => "admins#newsletter", via: [:get, :post]
   
 end
