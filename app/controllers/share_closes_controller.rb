5acbe1efa15b488755c790cc81060e3d5c985dec
Ed Hamilton <madebyeggs@gmail.com>
2018-03-06 10:08:42 +0000

    comment

--- app/controllers/share_closes_controller.rb
+++
@@ -1,2 +0,0 @@
-class ShareClosesController < ApplicationController
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131172937) do
+ActiveRecord::Schema.define(version: 20180306100218) do
 
   create_table "announcements", force: :cascade do |t|
     t.text     "slide_title"
@@ -47,85 +47,10 @@ ActiveRecord::Schema.define(version: 20180131172937) do
 
   add_index "announcements", ["slug"], name: "index_announcements_on_slug", unique: true
 
-  create_table "artists", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "soundcloud"
-    t.string   "fb_url"
-    t.string   "twitter_name"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-    t.string   "fb_image_file_name"
-    t.string   "fb_image_content_type"
-    t.integer  "fb_image_file_size"
-    t.datetime "fb_image_updated_at"
-    t.integer  "row_order"
-    t.string   "slug"
-    t.boolean  "slideshow"
-    t.string   "slide_image_file_name"
-    t.string   "slide_image_content_type"
-    t.integer  "slide_image_file_size"
-    t.datetime "slide_image_updated_at"
-    t.text     "slide_title"
-    t.string   "vimeo"
-    t.text     "slide_title2"
-    t.text     "slide_title3"
-    t.integer  "Slideshowposition"
-  end
-
-  add_index "artists", ["slug"], name: "index_artists_on_slug", unique: true
-
-  create_table "clogos", force: :cascade do |t|
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
-
   create_table "compositonlogos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "customs", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "vimeo_1"
-    t.string   "vimeo_2"
-    t.string   "vimeo_3"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-    t.string   "slug"
-    t.string   "genre"
-    t.integer  "row_order"
-    t.string   "soundcloud"
-    t.text     "fullqual"
-    t.text     "lowqual"
-    t.boolean  "display"
-    t.integer  "Displayposition"
-    t.string   "work_brand_name"
-    t.string   "work_campaign_title"
-    t.string   "work_brand_name2"
-    t.string   "work_campaign_title2"
-    t.string   "work_brand_name3"
-    t.string   "work_campaign_title3"
-    t.string   "platform"
-    t.string   "platform2"
-    t.string   "platform3"
-    t.string   "souncloudtype"
-  end
-
-  add_index "customs", ["slug"], name: "index_customs_on_slug", unique: true
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -139,73 +64,6 @@ ActiveRecord::Schema.define(version: 20180131172937) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
-
-  create_table "newsletters", force: :cascade do |t|
-    t.string   "title1"
-    t.string   "title2"
-    t.string   "facebook"
-    t.string   "twitter"
-    t.string   "instagram"
-    t.string   "website"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.string   "logo_file_name"
-    t.string   "logo_content_type"
-    t.integer  "logo_file_size"
-    t.datetime "logo_updated_at"
-    t.string   "mainimage_file_name"
-    t.string   "mainimage_content_type"
-    t.integer  "mainimage_file_size"
-    t.datetime "mainimage_updated_at"
-    t.string   "downloadlink"
-  end
-
-  create_table "releases", force: :cascade do |t|
-    t.string   "title"
-    t.string   "soundcloud"
-    t.text     "description"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-    t.string   "fb_image_file_name"
-    t.string   "fb_image_content_type"
-    t.integer  "fb_image_file_size"
-    t.datetime "fb_image_updated_at"
-    t.integer  "row_order"
-    t.string   "slug"
-    t.boolean  "slideshow"
-    t.string   "slide_image_file_name"
-    t.string   "slide_image_content_type"
-    t.integer  "slide_image_file_size"
-    t.datetime "slide_image_updated_at"
-    t.text     "slide_title"
-    t.string   "vimeo"
-    t.text     "slide_title2"
-    t.text     "slide_title3"
-    t.integer  "Slideshowposition"
-    t.boolean  "newsletter"
-    t.integer  "newsletterposition"
-    t.string   "downloadlink"
-  end
-
-  add_index "releases", ["slug"], name: "index_releases_on_slug", unique: true
-
-  create_table "researches", force: :cascade do |t|
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "title"
-    t.string   "slug"
-    t.integer  "row_order"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
-
-  add_index "researches", ["slug"], name: "index_researches_on_slug", unique: true
 
   create_table "slides", force: :cascade do |t|
     t.datetime "created_at", null: false
