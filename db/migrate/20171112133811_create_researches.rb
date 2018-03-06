26e2eec35a36bfb69f61af32c79625e52fdb7ab7
Ed Hamilton <madebyeggs@gmail.com>
2018-03-06 10:10:14 +0000

    comment

--- db/migrate/20171112133811_create_researches.rb
+++
@@ -1,8 +0,0 @@
-class CreateResearches < ActiveRecord::Migration
-  def change
-    create_table :researches do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20180131172908_create_clogos.rb
+++
@@ -1,8 +0,0 @@
-class CreateClogos < ActiveRecord::Migration
-  def change
-    create_table :clogos do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
