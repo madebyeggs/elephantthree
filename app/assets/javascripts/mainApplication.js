ba8add51d08865558f2f81b2d6f7ff812d140a92
Ed Hamilton <madebyeggs@gmail.com>
2018-04-04 15:12:30 +0100

    bg image fadeout on click

--- app/assets/javascripts/mainApplication.js
+++ app/assets/javascripts/mainApplication.js
@@ -17,33 +17,13 @@
 //= require Footer.js
 //= require jquery.fitvids.js
 
-require.config({
-	"waitSeconds": 0,
-	"paths": {
-		"root": "http://hsachs.at/",
-		"base": "http://hsachs.at/Base/Js/",
-		"project": "http://hsachs.at/Assets/Js/",
-		"jquery": "http://hsachs.at/Base/Js/Vendor/JQuery",
-		"async": "http://hsachs.at/Base/Js/Vendor/Async",
-		"flickity": "http://hsachs.at/Assets/Js/Vendor/Flickity",
-		"debounce": "http://hsachs.at/Assets/Js/Vendor/debounce",
-		"waypoint": "http://hsachs.at/Assets/Js/Vendor/waypoint"
-	},
-	"shim": {
-		"flickity": [
-    		"jquery"
-		],
-		"debounce": [
-    		"jquery"
-		],
-		"waypoint": [
-    		"jquery"
-		]
-	}
-});
-
 $(document).ready(function(){
 	$(".iframe").fitVids();
+	
+	$('.bgImage').on('click',function(){
+	    var image = $(this).data('image');
+	    $('.bgImage').fadeOut('3000');
+	});
 });
 
 function loadIframe(iframeName, url) {
